cdb6aa87e858da7ec106179631ce47ca3ad45853
Jenni <jenni@ohsomodern.org>
2021-12-28 20:04:45 +0200

    ✨ Hero

---
+++ src/components/organisms/Hero/Hero.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="hero">
+    <img
+      src="~@/assets/decor/matateljen_hero.jpg"
+      srcset="
+        ~@/assets/decor/matateljen_hero_mobile.jpg  320w,
+        ~@/assets/decor/matateljen_hero.jpg         800w,
+        ~@/assets/decor/matateljen_hero.jpg        1200w
+      "
+      sizes="(min-width: 60rem) 80vw, (min-width: 40rem) 90vw, 100vw"
+      alt="Charkbricka med två kaffemuggar"
+    />
+    <div class="gradient"></div>
+
+    <div class="centered-content">
+      <blockquote>
+        <div class="blur-bg"></div>
+        <h1>Hos oss får du närodlad mat i hjärtat av Gamla Enskede!</h1>
+        <cite>Henrik Sauer, grundare</cite>
+      </blockquote>
+
+      <div class="buttons">
+        <!-- todo: popup -->
+        <Button button-text="Boka bord" />
+        <Button button-text="Catering" button-style="secondary" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Button from '@/components/atoms/Button/Button';
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Hero',
+  components: { Button },
+};
+</script>
+
+<style lang="scss">
+@import 'hero';
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,73 @@
 <template>
-  <section>home</section>
-    <div>hero</div>
+  <section class="view-home">
+    <Hero />
+    <section class="food-menu">
+      <div class="wave">
+        <img src="~@/assets/decor/wave.svg" alt="" />
+      </div>
+      <div class="food-menu-content">
+        <h2>Vår meny</h2>
+        <div class="inner">
+          <section>
+            <h3>Smårätter</h3>
+            <ul>
+              <li>
+                <h4>I säsong</h4>
+                <p>
+                  <span>Brysselkål, dragon&shy;pistou & pecorino</span>
+                  <span class="price">115 kr</span>
+                </p>
+              </li>
+              <li>
+                <h4>Ost</h4>
+                <p>
+                  <span>Burrata, fermenterad rädisa & frömix</span>
+                  <span class="price">135 kr</span>
+                </p>
+              </li>
+              <li>
+                <h4>Fisk</h4>
+                <p>
+                  <span>Regnbågsrom & lagerblads&shy;emulsion</span>
+                  <span class="price">155 kr</span>
+                </p>
+              </li>
+            </ul>
+            <h4></h4>
+          </section>
+          <section>
+            <h3>Dessert</h3>
+            <ul>
+              <li>
+                <h4>Mousse</h4>
+                <p>
+                  <span>Vispad saffransmousse, rostad vit choklad & granité på lingon</span>
+                  <span class="price">125 kr</span>
+                </p>
+              </li>
+              <li>
+                <h4>Mormors klassiker</h4>
+                <p>
+                  <span>Varma hjortron, fikonblads&shy;färskost, kokos & mandel</span>
+                  <span class="price">135 kr</span>
+                </p>
+              </li>
+            </ul>
+            <h4></h4>
+          </section>
+        </div>
+      </div>
+    </section>
+  </section>
 </template>
+
+<style lang="scss">
+@import '~@/styles/views/home_view.scss';
+</style>
+<script>
+import Hero from '@/components/organisms/Hero/Hero';
+
+export default {
+  components: { Hero },
+};
+</script>
