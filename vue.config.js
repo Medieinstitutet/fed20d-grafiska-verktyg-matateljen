65ee1ae4499abfee8c4b43b1ab79dae5e1d9ac5d
Jenni <jenni@ohsomodern.org>
2021-12-28 12:38:48 +0200

    🎉 Setup project base

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+  filenameHashing: true,
+  productionSourceMap: false,
+  css: {
+    sourceMap: false,
+    extract: false,
+    loaderOptions: {
+      sass: {
+        // Whatever you do, do not remove the semicolon in the import statement below, or 💥
+        additionalData: '@import "@/styles/config/_variables.scss";',
+      },
+    },
+  },
 });
