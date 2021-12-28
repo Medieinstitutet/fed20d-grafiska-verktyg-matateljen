0daeca02894d9985eb2baa9be40520bd652ddc87
Jenni <jenni@ohsomodern.org>
2021-12-28 15:00:56 +0200

    ✨ Button

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button :class="buttonCSSClass">
+    {{ buttonText }}
+  </button>
+</template>
+
+<style lang="scss">
+@import 'button';
+</style>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Button',
+  computed: {
+    buttonCSSClass() {
+      return `btn-${this.buttonStyle}`;
+    },
+  },
+  props: {
+    buttonText: {
+      type: String,
+      default: '',
+    },
+    buttonStyle: {
+      type: String,
+      default: 'primary', // TODO - Not working
+      validator(value) {
+        return ['primary', 'secondary', 'contrast', 'disabled'].includes(value);
+      },
+    },
+  },
+};
+</script>
