b44aa91628ea74b5588e79205468b48d0186e0bd
Jenni <jenni@ohsomodern.org>
2021-12-28 15:35:48 +0200

    ✨ Cookie Banner

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
-  <router-view />
+  <header>logga + meny</header>
+  <main>
+    <router-view />
+  </main>
+  <footer>footer-elementet</footer>
+  <CookieBanner />
   <!--  <nav>-->
   <!--    <router-link to="/">Home</router-link> |-->
   <!--    <router-link to="/about">About</router-link>-->
   <!--  </nav>-->
   <!--  <router-view />-->
 </template>
+
+<script>
+import CookieBanner from '@/components/CookieBanner/CookieBanner.vue';
+
+export default {
+  components: { CookieBanner },
+};
+</script>
---
+++ src/components/CookieBanner/CookieBanner.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="cookie-banner">
+    <div class="disclaimer-text">
+      <h5>Cookies</h5>
+      <p>
+        Vi använder cookies på den här sidan för att ge dig en bättre upplevelse. Vissa behövs för att sidan ska fungera
+        korrekt men andra kan du välja att godkänna.
+      </p>
+    </div>
+    <div class="buttons">
+      <Button button-text="Acceptera alla" button-style="contrast" />
+      <Button button-text="Acceptera endast nödvändiga" button-style="secondary" />
+    </div>
+
+    <div class="settings">
+      <p>Cookies-inställningar</p>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+@import '_cookie_banner.scss';
+</style>
+
+<script>
+import Button from '@/components/Button/Button';
+
+export default {
+  components: { Button },
+};
+</script>
